2f6ee9d8abd70cad5189cdd01e0c1ff168ce886d
Joseph Brown <joseph.d.brown94@gmail.com>
2020-02-13 00:21:25 -0500

    Created a custom user model in Django.

--- project/forms.py
+++ project/forms.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 
 from user.models import CustomUser
 
+
 class SignUpForm(UserCreationForm):
     class Meta:
         model = CustomUser
@@ -12,6 +13,7 @@ class SignUpForm(UserCreationForm):
             'username',
             'age'
         ]
+
 
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=30)
